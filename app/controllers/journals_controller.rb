f4b2d27a28611c26cd883fbc29fb47b2b6c7fd11
Takenori Takaki <takenory@gmail.com>
2016-04-28 16:57:32 +0900

    Addd permission checkking (set_notes_private)

--- app/controllers/journals_controller.rb
+++ app/controllers/journals_controller.rb
@@ -91,7 +91,8 @@ class JournalsController < ApplicationController
   def update
     (render_403; return false) unless @journal.editable_by?(User.current)
     @journal.notes = params[:notes] if params[:notes]
-    @journal.private_notes = (params[:private_notes] || false)
+    @journal.private_notes = params[:private_notes] if params[:private_notes]
+    (render_403; return false) if @journal.private_notes_changed? && User.current.allowed_to?(:set_notes_private, @journal.issue.project) == false
     @journal.save if @journal.changed?
     @journal.destroy if @journal.details.empty? && @journal.notes.blank?
     call_hook(:controller_journals_edit_post, { :journal => @journal, :params => params})
